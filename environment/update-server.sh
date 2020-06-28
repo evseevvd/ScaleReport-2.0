5587ce456c34aadaa451a928ddc74eda93d1fdba
v.evseev <v.evseev.d@gmail.com>
2020-06-28 22:14:54 +0700

    SCL-014: fix update script

--- environment/update-server.sh
+++ environment/update-server.sh
@@ -3,7 +3,7 @@
 workdir=/opt/scale-server;
 
 systemctl stop scale-server;
-cp -i scale-server-2.0.1.jar ${workdir};
+yes | cp -rf scale-server-2.0.1.jar ${workdir};
 chmod 775 ${workdir}/scale-server-2.0.1.jar;
 systemctl start scale-server;
 exit 0;
